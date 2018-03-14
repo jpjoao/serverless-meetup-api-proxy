a2bcf8875171116b2f67d2ac552c83257fec5f01
Gustavo Castilião <gucastiliao@gmail.com>
2018-03-13 21:50:29 -0300

    fix: fix problem with cors

--- src/call.js
+++ src/call.js
@@ -1,20 +1,25 @@
 const meetup = require('../services/meetup');
 
 module.exports.handler = (event, context, callback) => {
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': true,
+  };
+
   const meetupEvents = meetup(event.queryStringParameters, event.httpMethod);
 
   Promise.all([meetupEvents])
     .then(values => {
       callback(null, {
         statusCode: 200,
-        body: JSON.stringify({
-          data: values[0].data,
-        }),
+        headers,
+        body: JSON.stringify(values[0].data),
       });
     })
     .catch(error => {
       callback(null, {
         statusCode: 500,
+        headers,
         body: JSON.stringify({
           error: true,
           message: error,
